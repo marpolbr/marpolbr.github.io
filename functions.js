5c66d112bf0a6bf96ded96c4b4e407cf040773ed
Marcin <polbrat.marcin@gmail.com>
2016-09-17 13:09:12 +0200

    #1.5 Buttons Szczegoly

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     var daneOsobowe = "";
-    
+    //pobranie danych z bazy
     for (i=0;i<bazaOsob.osoba.length;i++){
         daneOsobowe += 
             "<tr>" +
@@ -9,17 +9,26 @@ $(document).ready(function(){
             "<td>" + bazaOsob.osoba[i].nazwisko + "</td>" +
             "<td>" + bazaOsob.osoba[i].email + "</td>" +
             "<td>" + bazaOsob.osoba[i].telefon + "</td>" +
-            "</tr>";  
-    };  
+            "<td><a href='widokszczeg.html'><Button type='button' class='btn btn-info btn-sm'>Szczegoly</Button></a></td>" +
+            "</tr>";
+    };
+//    "<td><a href='widokszczeg.html'><Button id='buttonSzczeg"+[i]+"'>Szczegoly</Button></a></td>" +
+$('#testSzczegoly').click(function(){
+    location.href='widokszczeg.html'
+})
     
+    //wyswietlenie danych w tabeli
     $("#tabelaOsob").html(
         "<tr>" +
         "<th>Imie</th>" +
         "<th>Nazwisko</th>" +
         "<th>Email</th>" +
         "<th>Telefon</th>" +
+        "<th>Opcje</th>" +
         "</tr>" +
         daneOsobowe
         );
 
 });
+
+//<a href="widokszczeg.html"><button id="przyciskSzczegoly" type="button" class="btn btn-info">Widok szczegolowy</button></a>
