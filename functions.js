1f3b12a92a55a167031f0baa43bd99b6ff5297ae
Marcin <polbrat.marcin@gmail.com>
2016-09-17 17:33:01 +0200

    #1.7

--- functions.js
+++ functions.js
@@ -8,15 +8,11 @@ $(document).ready(function(){
             "<td>" + bazaOsob.osoba[i].imie + "</td>" +
             "<td>" + bazaOsob.osoba[i].nazwisko + "</td>" +
             "<td>" + bazaOsob.osoba[i].email + "</td>" +
-            "<td>" + bazaOsob.osoba[i].telefon + "</td>" +
-            "<td><a href='widokszczeg.html'><Button type='button' class='btn btn-info btn-sm'>Szczegoly</Button></a></td>" +
+            "<td>" + bazaOsob.osoba[i].telefon + "</td>" +            
+            "<td><a href='widokszczeg.html' id='linkWidokSzczegolowy'><Button id='btnSzczegoly"+[i]+"' type='button' class='testujese btn btn-info btn-sm'>Szczegoly</Button></a></td>" +
             "</tr>";
     };
-//    "<td><a href='widokszczeg.html'><Button id='buttonSzczeg"+[i]+"'>Szczegoly</Button></a></td>" +
-$('#testSzczegoly').click(function(){
-    location.href='widokszczeg.html'
-})
-    
+
     //wyswietlenie danych w tabeli
     $("#tabelaOsob").html(
         "<tr>" +
@@ -27,8 +23,43 @@ $('#testSzczegoly').click(function(){
         "<th>Opcje</th>" +
         "</tr>" +
         daneOsobowe
-        );
+    );
+    
+        
+//
+//        $("#btnSzczegoly"+[i]).click(function(){
+//            var strLink = "widokszczeg.html?" + i;
+//            $("#linkWidokSzczegolowy").attr("href",strLink);
+//        });
+    
+//    $("#btnSzczegoly0").click(function() {
+//    var contentPanelId = $(this).attr("id");
+//    alert(contentPanelId);
+//});
+    
+//        $("#btnSzczegoly0").click(function(event) {
+//        // this.append wouldn't work
+//        $(this).append(" Clicked");
+//    });
+    
+        $("a").click(function(event) {
+        alert(event.target.id);
+    });
+    
+        
+
+    
+    //    var daneTest = "";
+//    $("#testSzczeg").html("<p>" + bazaOsob.osoba[scrt_var].imie + " " + bazaOsob.osoba[scrt_var].nazwisko + "<p>");
+
+//        var scrt_var = 0;
+//    $("#btnSzczegoly0").click(function(){
+//        var strLink = "widokszczeg.html?" + scrt_var;
+//        $("#linkWidokSzczegolowy").attr("href",strLink);
+//    });
+    
+
+    
 
 });
 
-//<a href="widokszczeg.html"><button id="przyciskSzczegoly" type="button" class="btn btn-info">Widok szczegolowy</button></a>
